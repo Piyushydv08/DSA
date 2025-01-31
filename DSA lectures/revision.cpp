0ce313e122c103f44467c469542c00cf6b528da7
Piyush Yadav <piyushydv011@gmail.com>
2025-02-01 00:11:37 +0530

    new

--- DSA lectures/revision.cpp
+++ DSA lectures/revision.cpp
@@ -1,13 +1,30 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-
-int main() {
-    for(int i=0; i<5; i++) {
-        for(int j=i; j<=5; j++) {
-            if(i+j == 10) {
-                break;
-            }
-            cout <<i<< " " << j << endl;
+int fibonacci(int n){
+    if(n==1){
+        return 0;
+    }
+    else if(n==2){
+        return 1;
+    }
+    else if(n<1){
+        cout<<"enter a valid value which is greater than zero";
+    }
+    else{
+        int a=0;
+        int b=1;
+        int c = 0;
+        for(int i=2;i<n;i++){
+        c=a+b;
+        a=b;
+        b=c;
         }
+        cout << c;
     }
-    }
+}
+int main(){
+    int n;
+    cout<<"enter n:";
+    cin>>n;
+    return fibonacci(n);
+}  
