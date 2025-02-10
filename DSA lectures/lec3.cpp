dbfeb88d9adbe55a233d63c30b0ca2ed1653bfac
Piyush Yadav <piyushydv011@gmail.com>
2025-02-11 00:43:31 +0530

    lec4 rev done

--- DSA lectures/lec3.cpp
+++ DSA lectures/lec3.cpp
@@ -4,10 +4,16 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    int i = 1;
-    while(i<=n) {
-        cout<<i<<endl;
-        i = i+1;
+
+    int i=2;
+    while(i<n){
+        if(n%i==0){
+            cout<<"Not Prime"<< i << endl;
+        }
+        else{
+            cout<<"Prime"<< i << endl;
+        }
+        i++;
     }
 
 
--- DSA lectures/lec4.cpp
+++ DSA lectures/lec4.cpp
@@ -5,17 +5,54 @@ int main() {
   int n;
   cin>>n;
   
-  int i= 1;
-   while(i<=n){
+  // int i= 1;
+  //  while(i<=n){
+  //   int j = 1;
+  //   while(j<=1){
+  //       char ch = 'A' + i +j - 2;
+  //       cout<<ch;
+  //       j++;
+  //   }
+  //   cout<<endl;
+  //   i++; 
+  //  }
+
+  // int i = 1;
+  // while(i<=n){
+  //   int space = n- i;
+  //   while(space){
+  //     cout<<" ";
+  //     space--;
+  //   }
+  //   int j = 1;
+  //   while(j<=i){
+  //     cout<<"*";
+  //     j++;
+  //   }
+  //   cout<<endl;
+  //   i++;
+  // }
+
+  int i = 1;
+  while(i<=n){
+    int space = n- i;
+    while(space){
+      cout<<" ";
+      space--;
+    }
     int j = 1;
-    while(j<=n){
-        char ch  = 'A' +j-1;
-        cout<<ch;
-        j++;
+    while(j<=i){
+      cout<<j;
+      j++;
+    }
+    int start = i-1;
+    while(start>=1){
+      cout<<start;
+      start--;
     }
     cout<<endl;
-    i++; 
-   }
+    i++;
+  }
 }
 
     
