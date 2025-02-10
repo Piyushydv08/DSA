16a4d82d51b7bf69a702ac61944cc00c2ba841cb
Piyush Yadav <piyushydv011@gmail.com>
2025-02-11 00:58:35 +0530

    lec5 rev. done

--- DSA lectures/tempCodeRunnerFile.cpp
+++ DSA lectures/tempCodeRunnerFile.cpp
@@ -1,8 +1,4 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    // int a = 4;
+int a = 4;
     // int b = 6;
 
     // cout << "a & b - " << (a & b) << endl;
@@ -14,12 +10,3 @@ int main() {
     // cout << (17 >> 1) << endl;
     // cout << (17 >> 2) << endl;
     // cout << (17 >> 3) << endl;
-
-    int n;
-    cin >> n;
-     int sum = 0;
-    for(int i = 0; i < n; i++) {
-        sum += i;
-    }
-    cout << sum << endl;
-}
